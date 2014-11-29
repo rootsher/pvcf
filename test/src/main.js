08517b8cc97af75922ad40704fc333bdd0cba5fa
Konrad Kowalski <rootsher@gmail.com>
2014-11-29 19:47:18 +0100

    Extend initialize layer by pattern manager.

--- test/src/main.js
+++ test/src/main.js
@@ -10,19 +10,43 @@ requirejs.config({
 
 requirejs(['pvcf'], function run(pvcf) {
     var definitions = pvcf.definitions;
+
     var ViewDefinition = definitions.ViewDefinition;
+    var PatternDefinition = definitions.PatternDefinition;
+
+    var PatternManager = pvcf.PatternManager;
     var TabManager = pvcf.TabManager;
 
-    // ### View list. ###
+    // ### View list ###
 
     // # Events #
 
     var addEvent = new ViewDefinition('test/templates/addEvent.html', ['test/modules/addEvent.js'], true);
+    var showEvent = new ViewDefinition('test/templates/showEvent.html', ['test/modules/showEvent.js'], true);
+
+
+    // # Pattern manager #
+
+    var patternManager = new PatternManager();
+
+    // ### Pattern list ###
+
+    patternManager.addPattern(new PatternDefinition('event/add', addEvent));
+    patternManager.addPattern(new PatternDefinition('event/show/:id', showEvent));
 
 
     // # Tab manager #
 
     var tabManager = new TabManager();
+    document.body.appendChild(tabManager.getContainerElement());
+
+
+    function main() {
+        console.log('Appliaction started!');
+    }
+
+
+    // window.onload actions:
     var startTab = tabManager.openTab();
 
     startTab.openView(addEvent).done(function () {
@@ -31,5 +55,9 @@ requirejs(['pvcf'], function run(pvcf) {
 
     tabManager.getContainerElement().appendChild(startTab.getContainerElement());
 
-    document.body.appendChild(tabManager.getContainerElement());
+    main();
+
+
+    // window.hashchange actions:
+    window.addEventListener('hashchange', main);
 });
